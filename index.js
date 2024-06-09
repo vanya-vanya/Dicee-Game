fee3c3147a9a06f682f810a5ef5d116ae33a0d39
Ivan Nosenko <frenafrani@gmail.com>
2024-06-09 12:49:25 +0300

    Complete Dicee Project

---
+++ index.js
@@ -0,0 +1,13 @@
+var randomNumber1 = "images/dice" + (Math.floor(Math.random() * 6 + 1)) + ".png";
+var randomNumber2 = "images/dice" + (Math.floor(Math.random() * 6 + 1)) + ".png";
+
+document.querySelector(".img1").setAttribute("src", randomNumber1);
+document.querySelector(".img2").setAttribute("src", randomNumber2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!🚩";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
